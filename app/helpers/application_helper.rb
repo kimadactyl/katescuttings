4548d84320da8c7e2c37bbcb5e56294985cfdaf5
Dr Kim Foale <kim@gfsc.studio>
2020-04-23 13:02:22 +0100

    Begin adding blog styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def markdown(content)
+    return "" if content.class != String
+    options = {
+      autolink: true,
+      no_intra_emphasis: true,
+      lax_html_blocks: true,
+      strikethrough: true,
+      superscript: true,
+      hard_wrap: true
+    }
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML, options).render(content).html_safe
+  end
 end
