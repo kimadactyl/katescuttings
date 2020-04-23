841572b36a7cfdd52e25b2f122224c199f558446
Dr Kim Foale <kim@gfsc.studio>
2020-04-23 15:50:36 +0100

    Add loads of blog stuff, images, styles etc

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,7 @@
 class BlogsController < ApplicationController
   def index
-    @blogs = Blog.order(created_at: :desc).limit(10)
+    @blogs = Blog.teasers_sorted_by_month
+    @almanac = Blog.almanac
   end
 
   def show
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,20 @@
 class Blog < ApplicationRecord
+  def self.teasers_sorted_by_month(idx = 0)
+    blogs = Blog.order(created_at: :desc).limit(10).offset(idx)
+    blogs.group_by{ |b| b.created_at.strftime '%B %Y' }
+  end
+
+  def self.almanac
+    # Get all blogs in reverse date order
+    blogs = Blog.order(created_at: :desc).limit(50)
+    # Group them into months [["Year", "Month], Blog]]
+    months = blogs.group_by{ |b| [b.created_at.strftime('%Y'), b.created_at.strftime('%B')] }
+    # Group them into years [["Year"], [["Year", "Month], Blog], ["Year", "Month], Blog]]]
+    months.group_by{ |m| m.first.first }
+  end
+
+  def formatted_date
+    # Add time back in with  - %l:%M%p
+    created_at.strftime('%e %B %Y')
+  end
 end
