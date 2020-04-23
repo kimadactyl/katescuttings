8cc9bc87e7033ae8914205d44bd1f7632805eb30
Dr Kim Foale <kim@gfsc.studio>
2020-04-23 17:40:19 +0100

    Start fixing up ActiveStorage

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,16 +1,19 @@
 class Blog < ApplicationRecord
+  validates :title, presence: true
+  has_many_attached :images
+
   def self.teasers_sorted_by_month(idx = 0)
     blogs = Blog.order(created_at: :desc).limit(10).offset(idx)
-    blogs.group_by{ |b| b.created_at.strftime '%B %Y' }
+    blogs.group_by { |b| b.created_at.strftime '%B %Y' }
   end
 
   def self.almanac
     # Get all blogs in reverse date order
     blogs = Blog.order(created_at: :desc).limit(50)
     # Group them into months [["Year", "Month], Blog]]
-    months = blogs.group_by{ |b| [b.created_at.strftime('%Y'), b.created_at.strftime('%B')] }
+    months = blogs.group_by { |b| [b.created_at.strftime('%Y'), b.created_at.strftime('%B')] }
     # Group them into years [["Year"], [["Year", "Month], Blog], ["Year", "Month], Blog]]]
-    months.group_by{ |m| m.first.first }
+    months.group_by { |m| m.first.first }
   end
 
   def formatted_date
