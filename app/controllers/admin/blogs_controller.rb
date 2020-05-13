054f7d1cb6c5dd14739728a76a53cb978495a853
Dr Kim Foale <kim@gfsc.studio>
2020-05-13 14:45:13 +0100

    Add proper blog index and routes

--- app/controllers/admin/blogs_controller.rb
+++ app/controllers/admin/blogs_controller.rb
@@ -1,11 +1,46 @@
 class Admin::BlogsController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+  before_action :set_blog, only: %i[edit update destroy]
 
   def index
-    @blogs = Blog.order(created_at: :desc).limit(20)
+    @blogs = Blog.order(created_at: :desc).page(params[:page]).per(25)
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+
+    if @blog.save
+      flash[:success] = "Successfully created new blog"
+      redirect_to admin_blogs_path
+    else
+      render 'new'
+    end
   end
 
   def edit
+  end
+
+  def update
+    @blog.update(blog_params)
+    redirect_to admin_blogs_path
+  end
+
+  def destroy
+    @blog.destroy
+    redirect_to admin_blogs_path
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :body, :images)
+  end
+
+  def set_blog
     @blog = Blog.find(params[:id])
   end
 end
